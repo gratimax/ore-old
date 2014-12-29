24a7dc5d662e4f6a91a1740d470663c07ac4215f
gratimax <max@ovsankin.com>
2014-12-28 17:37:11 -0800

    Use docker-provided env variables for db

--- repo/settings/settings_production.py
+++ repo/settings/settings_production.py
@@ -7,6 +7,6 @@ settings['cookie_secret'] = os.environ.get('SECRET_KEY')
 
 settings['db_user'] = os.environ.get('DB_USER')
 settings['db_password'] = os.environ.get('DB_PASSWORD')
-settings['db_host'] = os.environ.get('DB_HOST')
-settings['db_port'] = os.environ.get('DB_PORT')
+settings['db_host'] = os.environ.get('DB_PORT_5432_TCP_ADDR')
+settings['db_port'] = os.environ.get('DB_PORT_5432_TCP_PORT')
 settings['db_db'] = os.environ.get('DB_DB')
