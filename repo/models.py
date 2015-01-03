f082cbd9bb6cc93caae501ecece70fe195af8463
Luke Granger-Brown <git@lukegb.com>
2015-01-03 03:45:27 +0000

    Adds prototype permissions system

--- repo/models.py
+++ repo/models.py
@@ -4,11 +4,16 @@ from django.core.mail import send_mail
 from django.db import models
 from django.utils.translation import ugettext_lazy as _t
 from django.utils import timezone
+from django.db.models.signals import post_save
+
+from model_utils.managers import InheritanceManager
 
 
 EXTENDED_CHAR_REGEX = r'[\w.@+-]+'
 EXTENDED_NAME_REGEX = r'^' + EXTENDED_CHAR_REGEX + r'$'
 TRIM_NAME_REGEX = r'^' + EXTENDED_CHAR_REGEX + r'([\w.@+ -]*' + EXTENDED_CHAR_REGEX + r')?$'
+
+Q = models.Q
 
 
 class Namespace(models.Model):
@@ -17,6 +22,9 @@ class Namespace(models.Model):
                             validators=[
                                 validators.RegexValidator(EXTENDED_NAME_REGEX, 'Enter a namespace organization name.', 'invalid')
                             ])
+
+    objects = InheritanceManager()
+
 
 class RepoUserManager(UserManager):
     def _create_user(self, username, email, password, is_staff, is_superuser, **extra_fields):
@@ -51,10 +59,16 @@ class RepoUser(AbstractBaseUser, PermissionsMixin, Namespace):
     def email_user(self, subject, message, from_email=None, **kwargs):
         send_mail(subject, message, from_email, [self.email], **kwargs)
 
+    def user_has_permission(self, user, perm_slug, project=None):
+        return user == self
+
 
 class Organization(Namespace):
 
-    pass
+    def user_has_permission(self, user, perm_slug, project=None):
+        if self.teams.filter(users=user).filter(Q(is_all_projects=True) | Q(projects=project)).filter(Q(is_owner_team=True) | Q(permissions__slug=perm_slug)).count():
+            return True
+        return False
 
 
 class Project(models.Model):
@@ -65,6 +79,11 @@ class Project(models.Model):
                             ])
     namespace = models.ForeignKey(Namespace, related_name='projects')
     description = models.TextField('description')
+
+    def user_has_permission(self, user, perm_slug):
+        if self.teams.filter(users=user).filter(Q(is_owner_team=True) | Q(permissions__slug=perm_slug)).count():
+            return True
+        return Namespace.objects.get_subclass(id=self.namespace_id).user_has_permission(user, perm_slug, project=self)
 
 
 class Version(models.Model):
@@ -85,3 +104,68 @@ class File(models.Model):
                             ])
     description = models.TextField('description')
     version = models.ForeignKey(Version, related_name='files')
+
+
+class Permission(models.Model):
+    slug = models.SlugField(max_length=64)
+    name = models.CharField(max_length=64, null=False, blank=False)
+    description = models.TextField(null=False, blank=False)
+    applies_to_project = models.BooleanField(default=True)
+
+
+class Team(models.Model):
+    name = models.CharField('name', max_length=80, null=False, blank=False)
+    users = models.ManyToManyField(RepoUser, related_name='%(class)ss')
+    permissions = models.ManyToManyField(Permission, related_name='+')
+    is_owner_team = models.BooleanField(default=False)
+
+    def check_consistent(self):
+        return True
+
+    def make_consistent(self):
+        return
+
+    class Meta:
+        abstract = True
+
+
+class OrganizationTeam(Team):
+    organization = models.ForeignKey(Organization, related_name='teams')
+    projects = models.ManyToManyField(Project, related_name='organizationteams')
+    is_all_projects = models.BooleanField(default=False)
+
+    def make_consistent(self):
+        self.projects = self.projects.filter(namespace=self.organization)
+        self.save()
+
+    def check_consistent(self):
+        return self.projects.exclude(namespace=self.organization).count() == 0
+
+
+class ProjectTeam(Team):
+    project = models.ForeignKey(Project, related_name='teams')
+
+    # TODO: we need to check here that if we're a user's project and we're the owner team, that that user is in us!
+
+def create_project_owner_team(sender, instance, created, **kwargs):
+    if instance and created:
+        owning_namespace = Namespace.objects.get_subclass(id=instance.namespace_id)
+        if isinstance(owning_namespace, RepoUser):
+            team = ProjectTeam.objects.create(
+                    project=instance,
+                    is_owner_team=True,
+                    name='Owners',
+            )
+            team.users = [owning_namespace]
+            team.save()
+post_save.connect(create_project_owner_team, sender=Project)
+
+def create_organization_owner_team(sender, instance, created, **kwargs):
+    if instance and created:
+        OrganizationTeam.objects.create(
+                name='Owners',
+                organization=instance,
+                is_all_projects=True,
+                is_owner_team=True,
+        )
+post_save.connect(create_organization_owner_team, sender=Organization)
--- repo/tests.py
+++ repo/tests.py
@@ -1,3 +1,57 @@
 from django.test import TestCase
+from . import models
 
 # Create your tests here.
+
+class PermissionsTestCase(TestCase):
+    def setUp(self):
+        self.user_john = models.RepoUser.objects.create_user('john', 'password', 'john@ore.spongepowered.org')
+        self.user_sally = models.RepoUser.objects.create_user('sally', 'password', 'sally@ore.spongepowered.org')
+        self.user_sue = models.RepoUser.objects.create_user('sue', 'password', 'sue@ore.spongepowered.org')
+
+        self.organization_sponge = models.Organization.objects.create(name='Sponge')
+
+        self.permission_foo = models.Permission.objects.create(slug='foo.do', name='Do Foo', description='Performs foo')
+        self.permission_bar = models.Permission.objects.create(slug='foo.bar', name='Bar', description='Bars foo')
+        self.permission_baz = models.Permission.objects.create(slug='baz.do', name='Do Baz', description='Bazzes the widget')
+
+        self.project_sponge = models.Project.objects.create(name='Sponge', namespace=self.organization_sponge, description='SPONGE') 
+        self.project_sponge_api = models.Project.objects.create(name='SpongeAPI', namespace=self.organization_sponge, description='SPONGE') 
+        self.project_bookit = models.Project.objects.create(name='Bookit', namespace=self.user_john, description='BOOkit')
+        self.project_nar = models.Project.objects.create(name='Nar', namespace=self.user_sally, description='NAR')
+
+        self.oteam_owners = self.organization_sponge.teams.get(is_owner_team=True, organization=self.organization_sponge)
+        self.oteam_owners.users = [self.user_john]
+
+        self.oteam_peons = models.OrganizationTeam.objects.create(name='Peons', organization=self.organization_sponge)
+        self.oteam_peons.users = [self.user_sue]
+        self.oteam_peons.projects = [self.project_sponge]
+        self.oteam_peons.permissions = [self.permission_foo]
+
+        self.pteam_serpents = models.ProjectTeam.objects.create(name='Serpents', project=self.project_sponge)
+        self.pteam_serpents.users = [self.user_sue, self.user_sally]
+        self.pteam_serpents.permissions = [self.permission_bar, self.permission_foo]
+
+    def test_organization_owner_team(self):
+        self.assertTrue(self.project_sponge.user_has_permission(self.user_john, 'foo.do'), 'John can foo.do')
+        self.assertTrue(self.project_sponge.user_has_permission(self.user_john, 'foo.bar'), 'John can foo.bar')
+        self.assertTrue(self.project_sponge.user_has_permission(self.user_john, 'baz.do'), 'John can baz.do')
+
+    def test_organization_teams(self):
+        self.assertTrue(self.project_sponge.user_has_permission(self.user_sue, 'foo.do'), 'Sue can foo.do on Sponge')
+        self.assertFalse(self.project_sponge_api.user_has_permission(self.user_sue, 'foo.do'), 'Sue can\'t foo.do on SpongeAPI')
+
+    def test_project_teams(self):
+        self.assertTrue(self.project_sponge.user_has_permission(self.user_sue, 'foo.bar'), 'Sue can foo.bar on Sponge')
+        self.assertTrue(self.project_sponge.user_has_permission(self.user_sally, 'foo.bar'), 'Sally can foo.bar on Sponge')
+
+        self.assertFalse(self.project_sponge_api.user_has_permission(self.user_sue, 'foo.bar'), 'Sue can\'t foo.bar on SpongeAPI')
+        self.assertFalse(self.project_sponge_api.user_has_permission(self.user_sally, 'foo.bar'), 'Sally can\'t foo.bar on SpongeAPI')
+
+        self.assertFalse(self.project_bookit.user_has_permission(self.user_sue, 'foo.bar'), 'Sue can\'t foo.bar on Bookit')
+        self.assertFalse(self.project_bookit.user_has_permission(self.user_sally, 'foo.bar'), 'Sally can\'t foo.bar on Bookit')
+        self.assertFalse(self.project_bookit.user_has_permission(self.user_sue, 'baz.do'), 'Sue can\'t baz.do on Bookit')
+        self.assertFalse(self.project_bookit.user_has_permission(self.user_sally, 'baz.do'), 'Sally can\'t baz.do on Bookit')
+
+        self.assertFalse(self.project_sponge_api.user_has_permission(self.user_sue, 'baz.do'), 'Sue can\'t baz.do on SpongeAPI')
+        self.assertFalse(self.project_sponge_api.user_has_permission(self.user_sally, 'baz.do'), 'Sally can\'t baz.do on SpongeAPI')
