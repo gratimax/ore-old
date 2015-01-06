f1a6c9b52ea3b403dab0b3408e95e48687f19600
Luke Granger-Brown <git@lukegb.com>
2015-01-06 21:43:33 +0000

    Resolve migration clash

---
+++ repo/migrations/0017_merge.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import models, migrations
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('repo', '0016_auto_20150105_1200'),
+        ('repo', '0016_auto_20150105_2137'),
+    ]
+
+    operations = [
+    ]
