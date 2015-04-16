a7bb3c7569d04fd97c9cd657373279ffd69e426b
gratimax <max@ovsankin.com>
2015-04-16 02:17:45 -0700

    Add markdown compilation utility

---
+++ ore/util/markdown.py
@@ -0,0 +1,60 @@
+from markdown2 import markdown
+from bleach import clean
+
+# Our own custom ruleset for sanitized markdown
+
+ALLOWED_TAGS = [
+    'a',
+    'abbr',
+    'acronym',
+    'b',
+    'blockquote',
+    'code',
+    'div',
+    'em',
+    'i',
+    'li',
+    'ol',
+    'p',
+    'pre',
+    'span',
+    'strong',
+    'table',
+    'ul',
+]
+
+ALLOWED_ATTRIBUTES = {
+    '*': ['style', 'class', 'id'],
+    'a': ['href', 'title', 'rel'],
+    'abbr': ['title'],
+    'acronym': ['title'],
+    # NOTE: While bleach does a good job at cleaning out <img src="javascript:alert('XSS')"> hacks,
+    # perhaps some greater care should be taken
+    'img': ['src', 'alt']
+}
+
+ALLOWED_STYLES = [
+    'color',
+    'background-color',
+    'font-size',
+    'font-weight',
+    'font-family',
+    'float',
+    'width',
+    'height',
+    'border'
+]
+
+MARKDOWN_EXTRAS = [
+    'fenced-code-blocks',
+    'header-ids',
+    'markdown-in-html',
+    'spoilers',
+    'tables'
+]
+
+
+def compile(text):
+    md = markdown(text, extras=MARKDOWN_EXTRAS)
+    sanitized = clean(md, tags=ALLOWED_TAGS, attributes=ALLOWED_ATTRIBUTES, styles=ALLOWED_STYLES)
+    return sanitized
