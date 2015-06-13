b63c07daf9896e8515c30e1f5eefbd483200039a
gratimax <max@ovsankin.com>
2015-06-12 21:18:00 -0700

    Add Sentry
    Yes, you heard me right, there's now somewhat sane error handling!

--- ore/settings/staging.py
+++ ore/settings/staging.py
@@ -7,3 +7,11 @@ ALLOWED_HOSTS = [
 
 MEDIA_URL = "/media/"
 MEDIA_ROOT = os.path.join(os.path.dirname(BASE_DIR), "media")
+
+RAVEN_CONFIG = {
+    'dsn': from_env('SENTRY_DSN', 'http://b7c76c65c6424ddf885765dafcf2173a:4f9dbb0bfd15467a94679e12abfdab48@sentry:9000/2'),
+}
+
+INSTALLED_APPS += (
+    'raven.contrib.django.raven_compat',
+)
