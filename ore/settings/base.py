6fad2d09badb3233793f384cd234329eaa36463e
gratimax <max@ovsankin.com>
2015-06-24 11:57:18 -0700

    Some more tinkering, wip

--- ore/settings/base.py
+++ ore/settings/base.py
@@ -37,7 +37,6 @@ AUTH_USER_MODEL = 'accounts.OreUser'
 # Application definition
 
 INSTALLED_APPS = (
-    'actstream',
     'grappelli',
     'django.contrib.admin',
     'django.contrib.auth',
@@ -48,6 +47,8 @@ INSTALLED_APPS = (
 
     'compressor',
     'djangobower',
+
+    'actstream',
 
     'crispy_forms',
     'reversion',
@@ -134,7 +135,16 @@ BOWER_INSTALLED_APPS = (
 )
 
 # Templates
-TEMPLATE_DIRS = ()
+TEMPLATES = [
+    {
+        'BACKEND': 'django.template.backends.django.DjangoTemplates',
+        'DIRS': [],
+        'APP_DIRS': True,
+        'OPTIONS': {
+            'context_processors': TEMPLATE_CONTEXT_PROCESSORS
+        }
+    }
+]
 
 LOGIN_REDIRECT_URL = '/'
 
