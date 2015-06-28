81968029a0fdd19b5de780d6946263733186eb48
Luke Granger-Brown <git@lukegb.com>
2015-06-28 17:52:06 +0100

    Adds utility for generating a wheelset for a given requirement set

---
+++ utils/generate_wheels.sh
@@ -0,0 +1,35 @@
+#!/bin/bash
+
+SCRIPT=$(readlink -f "$0")
+SCRIPTPATH=$(dirname "$SCRIPT")
+REQUIREMENTSPATH=$(dirname "$SCRIPTPATH")/requirements
+OUTPUTDIR=$(pwd)/wheels-${ENVIRONMENT}
+
+ENVIRONMENT=base
+if [ "$#" -eq 1 ]; then
+	ENVIRONMENT="$1"
+elif [ "$#" -eq 2 ]; then
+	ENVIRONMENT="$1"
+	OUTPUTDIR="$2"
+elif [ "$#" -eq 0 ]; then
+	:
+else
+	echo "$0" \[build_environment\] \[output_dir\]
+	exit 3
+fi
+
+ENVIRONMENT_FILE="${REQUIREMENTSPATH}/${ENVIRONMENT}.txt"
+
+echo Building wheels for $ENVIRONMENT from $ENVIRONMENT_FILE
+if [ ! -f $ENVIRONMENT_PATH ]; then
+	echo No such environment: $ENVIRONMENT
+	exit 1
+fi
+
+if [ -d $OUTPUTDIR ]; then
+	echo Output directory $OUTPUTDIR already exists
+	exit 2
+fi
+mkdir $OUTPUTDIR
+
+pip wheel --wheel-dir="$OUTPUTDIR" -r "$ENVIRONMENT_FILE"
