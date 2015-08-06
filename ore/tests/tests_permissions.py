ba8a122a844a108514e1afc858f0457f63780835
gratimax <max@ovsankin.com>
2015-08-05 23:47:23 -0700

    Fix permission tests

--- ore/tests/tests_permissions.py
+++ ore/tests/tests_permissions.py
@@ -1,3 +1,4 @@
+from django.contrib.contenttypes.models import ContentType
 from ore.accounts.models import OreUser
 from ore.core.models import Permission, Organization
 from django.test import TestCase
@@ -15,23 +16,31 @@ class PermissionsTestCase(TestCase):
         return user_john
 
     def setUp(self):
-        self.permission_foo = Permission.objects.create(
-            slug='foo.do', name='Do Foo', description='Performs foo')
-        self.permission_bar = Permission.objects.create(
-            slug='foo.bar', name='Bar', description='Bars foo')
-        self.permission_baz = Permission.objects.create(
-            slug='baz.do', name='Do Baz', description='Bazzes the widget')
+        org_content_type = ContentType.objects.get_for_model(Organization)
+        self.org_permission_foo = Permission.objects.create(
+            slug='org.foo.do', name='Do Foo', description='Performs foo', applies_to_model=org_content_type)
+        self.org_permission_bar = Permission.objects.create(
+            slug='org.foo.bar', name='Bar', description='Bars foo', applies_to_model=org_content_type)
+        self.org_permission_baz = Permission.objects.create(
+            slug='org.baz.do', name='Do Baz', description='Bazzes the widget', applies_to_model=org_content_type)
+        proj_content_type = ContentType.objects.get_for_model(Organization)
+        self.proj_permission_foo = Permission.objects.create(
+            slug='proj.foo.do', name='Do Foo', description='Performs foo', applies_to_model=proj_content_type)
+        self.proj_permission_bar = Permission.objects.create(
+            slug='proj.foo.bar', name='Bar', description='Bars foo', applies_to_model=proj_content_type)
+        self.proj_permission_baz = Permission.objects.create(
+            slug='proj.baz.do', name='Do Baz', description='Bazzes the widget', applies_to_model=proj_content_type)
 
     def test_unrelated_people_cant_do_anything_on_organization(self):
         organization_sponge = Organization.objects.create(name='Sponge')
         user_john = self.make_john()
 
         self.assertFalse(organization_sponge.user_has_permission(
-            user_john, 'foo.do'), 'John can\'t foo.do')
+            user_john, 'org.foo.do'), 'John can\'t foo.do')
         self.assertFalse(organization_sponge.user_has_permission(
-            user_john, 'foo.bar'), 'John can\'t foo.bar')
+            user_john, 'org.foo.bar'), 'John can\'t foo.bar')
         self.assertFalse(organization_sponge.user_has_permission(
-            user_john, 'baz.do'), 'John can\'t baz.do')
+            user_john, 'org.baz.do'), 'John can\'t baz.do')
 
     def test_unrelated_people_cant_do_anything_on_organization_project(self):
         organization_sponge = Organization.objects.create(name='Sponge')
@@ -41,11 +50,11 @@ class PermissionsTestCase(TestCase):
         user_john = self.make_john()
 
         self.assertFalse(project_sponge.user_has_permission(
-            user_john, 'foo.do'), 'John can\'t foo.do')
+            user_john, 'proj.foo.do'), 'John can\'t foo.do')
         self.assertFalse(project_sponge.user_has_permission(
-            user_john, 'foo.bar'), 'John can\'t foo.bar')
+            user_john, 'proj.foo.bar'), 'John can\'t foo.bar')
         self.assertFalse(project_sponge.user_has_permission(
-            user_john, 'baz.do'), 'John can\'t baz.do')
+            user_john, 'proj.baz.do'), 'John can\'t baz.do')
 
     def test_organization_owner_can_do_everything_on_organization(self):
         organization_sponge = Organization.objects.create(name='Sponge')
@@ -56,11 +65,11 @@ class PermissionsTestCase(TestCase):
         team.users = [user_john]
 
         self.assertTrue(organization_sponge.user_has_permission(
-            user_john, 'foo.do'), 'John can foo.do')
+            user_john, 'org.foo.do'), 'John can foo.do')
         self.assertTrue(organization_sponge.user_has_permission(
-            user_john, 'foo.bar'), 'John can foo.bar')
+            user_john, 'org.foo.bar'), 'John can foo.bar')
         self.assertTrue(organization_sponge.user_has_permission(
-            user_john, 'baz.do'), 'John can baz.do')
+            user_john, 'org.baz.do'), 'John can baz.do')
 
     def test_organization_owner_can_do_everything_on_project(self):
         organization_sponge = Organization.objects.create(name='Sponge')
@@ -74,11 +83,11 @@ class PermissionsTestCase(TestCase):
         team.users = [user_john]
 
         self.assertTrue(project_sponge.user_has_permission(
-            user_john, 'foo.do'), 'John can foo.do')
+            user_john, 'proj.foo.do'), 'John can foo.do')
         self.assertTrue(project_sponge.user_has_permission(
-            user_john, 'foo.bar'), 'John can foo.bar')
+            user_john, 'proj.foo.bar'), 'John can foo.bar')
         self.assertTrue(project_sponge.user_has_permission(
-            user_john, 'baz.do'), 'John can baz.do')
+            user_john, 'proj.baz.do'), 'John can baz.do')
 
     def test_project_owner_can_do_everything_on_project(self):
         user_john = self.make_john()
@@ -87,11 +96,11 @@ class PermissionsTestCase(TestCase):
         )
 
         self.assertTrue(project_sponge.user_has_permission(
-            user_john, 'foo.do'), 'John can foo.do')
+            user_john, 'proj.foo.do'), 'John can foo.do')
         self.assertTrue(project_sponge.user_has_permission(
-            user_john, 'foo.bar'), 'John can foo.bar')
+            user_john, 'proj.foo.bar'), 'John can foo.bar')
         self.assertTrue(project_sponge.user_has_permission(
-            user_john, 'baz.do'), 'John can baz.do')
+            user_john, 'proj.baz.do'), 'John can baz.do')
 
     def test_organization_all_project_teams_grant_permissions_on_projects(self):
         organization_sponge = Organization.objects.create(name='Sponge')
@@ -107,14 +116,14 @@ class PermissionsTestCase(TestCase):
             is_owner_team=False,
         )
         team.users = [user_john]
-        team.permissions = [self.permission_foo, self.permission_bar]
+        team.permissions = [self.proj_permission_foo, self.proj_permission_bar]
 
         self.assertTrue(project_sponge.user_has_permission(
-            user_john, 'foo.do'), 'John can foo.do')
+            user_john, 'proj.foo.do'), 'John can foo.do')
         self.assertTrue(project_sponge.user_has_permission(
-            user_john, 'foo.bar'), 'John can foo.bar')
+            user_john, 'proj.foo.bar'), 'John can foo.bar')
         self.assertFalse(project_sponge.user_has_permission(
-            user_john, 'baz.do'), 'John can\'t baz.do')
+            user_john, 'proj.baz.do'), 'John can\'t baz.do')
 
     def test_organization_all_project_teams_grant_permissions_on_organisations(self):
         organization_sponge = Organization.objects.create(name='Sponge')
@@ -127,14 +136,14 @@ class PermissionsTestCase(TestCase):
             is_owner_team=False,
         )
         team.users = [user_john]
-        team.permissions = [self.permission_foo, self.permission_bar]
+        team.permissions = [self.org_permission_foo, self.org_permission_bar]
 
         self.assertTrue(organization_sponge.user_has_permission(
-            user_john, 'foo.do'), 'John can foo.do')
+            user_john, 'org.foo.do'), 'John can foo.do')
         self.assertTrue(organization_sponge.user_has_permission(
-            user_john, 'foo.bar'), 'John can foo.bar')
+            user_john, 'org.foo.bar'), 'John can foo.bar')
         self.assertFalse(organization_sponge.user_has_permission(
-            user_john, 'baz.do'), 'John can\'t baz.do')
+            user_john, 'org.baz.do'), 'John can\'t baz.do')
 
     def test_organization_limited_project_teams_grant_permissions_on_selected_projects(self):
         organization_sponge = Organization.objects.create(name='Sponge')
@@ -150,15 +159,15 @@ class PermissionsTestCase(TestCase):
             is_owner_team=False,
         )
         team.users = [user_john]
-        team.permissions = [self.permission_foo, self.permission_bar]
+        team.permissions = [self.proj_permission_foo, self.proj_permission_bar]
         team.projects = [project_sponge]
 
         self.assertTrue(project_sponge.user_has_permission(
-            user_john, 'foo.do'), 'John can foo.do')
+            user_john, 'proj.foo.do'), 'John can foo.do')
         self.assertTrue(project_sponge.user_has_permission(
-            user_john, 'foo.bar'), 'John can foo.bar')
+            user_john, 'proj.foo.bar'), 'John can foo.bar')
         self.assertFalse(project_sponge.user_has_permission(
-            user_john, 'baz.do'), 'John can\'t baz.do')
+            user_john, 'proj.baz.do'), 'John can\'t baz.do')
 
     def test_organization_limited_project_teams_dont_grant_permissions_on_unselected_projects(self):
         organization_sponge = Organization.objects.create(name='Sponge')
@@ -177,15 +186,15 @@ class PermissionsTestCase(TestCase):
             is_owner_team=False,
         )
         team.users = [user_john]
-        team.permissions = [self.permission_foo, self.permission_bar]
+        team.permissions = [self.proj_permission_foo, self.proj_permission_bar]
         team.projects = [project_spongeapi]
 
         self.assertFalse(project_sponge.user_has_permission(
-            user_john, 'foo.do'), 'John can\'t foo.do')
+            user_john, 'proj.foo.do'), 'John can\'t foo.do')
         self.assertFalse(project_sponge.user_has_permission(
-            user_john, 'foo.bar'), 'John can\'t foo.bar')
+            user_john, 'proj.foo.bar'), 'John can\'t foo.bar')
         self.assertFalse(project_sponge.user_has_permission(
-            user_john, 'baz.do'), 'John can\'t baz.do')
+            user_john, 'proj.baz.do'), 'John can\'t baz.do')
 
     def test_organization_limited_project_teams_dont_grant_permissions_on_organisations(self):
         organization_sponge = Organization.objects.create(name='Sponge')
@@ -201,12 +210,12 @@ class PermissionsTestCase(TestCase):
             is_owner_team=False,
         )
         team.users = [user_john]
-        team.permissions = [self.permission_foo, self.permission_bar]
+        team.permissions = [self.org_permission_foo, self.org_permission_bar]
         team.projects = [project_sponge]
 
         self.assertFalse(organization_sponge.user_has_permission(
-            user_john, 'foo.do'), 'John can\'t foo.do')
+            user_john, 'org.foo.do'), 'John can\'t foo.do')
         self.assertFalse(organization_sponge.user_has_permission(
-            user_john, 'foo.bar'), 'John can\'t foo.bar')
+            user_john, 'org.foo.bar'), 'John can\'t foo.bar')
         self.assertFalse(organization_sponge.user_has_permission(
-            user_john, 'baz.do'), 'John can\'t baz.do')
+            user_john, 'orgl.baz.do'), 'John can\'t baz.do')
