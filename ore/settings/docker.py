92db21caea9da413c5de1e2109d5459592708942
gratimax <max@ovsankin.com>
2015-11-08 12:33:31 -0800

    Don't read APP-VERSION file at all

--- ore/settings/docker.py
+++ ore/settings/docker.py
@@ -9,9 +9,6 @@ INSTALLED_APPS += (
     'raven.contrib.django.raven_compat',
 )
 
-with open('APP-VERSION') as f:
-    app_version = f.readlines().strip()
-
 RAVEN_CONFIG = {
     'dsn': from_env('SENTRY_DSN')
 }
