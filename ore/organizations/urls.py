b673c613bbab12d53d9a0c9263cc2462082b78a7
Luke Granger-Brown <git@lukegb.com>
2016-01-03 23:44:14 +0000

    Forgot one

--- ore/organizations/urls.py
+++ ore/organizations/urls.py
@@ -4,7 +4,7 @@ from django.conf.urls import url, include
 from .views import OrganizationSettingsView
 
 urlpatterns = [
-    url(r'^organizations/(?P<organization_slug>' + EXTENDED_URL_REGEX + ')/settings/', include([
+    url(r'^organizations/(?P<namespace>' + EXTENDED_URL_REGEX + ')/settings/', include([
         url(r'^$', OrganizationSettingsView.as_view(),
             name='organizations-settings'),
     ])),
