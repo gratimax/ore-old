0f5dbb4c3be1c683b4b56a2f46066362443f9142
Luke Granger-Brown <git@lukegb.com>
2016-01-03 23:13:11 +0000

    Add redirect for users/ and organizations/ to namespace view

--- ore/urls.py
+++ ore/urls.py
@@ -9,6 +9,9 @@ import ore.versions.urls
 import ore.teams.urls
 import ore.discourse_sso.urls
 
+from django.views.generic import RedirectView
+from ore.core.regexs import EXTENDED_URL_REGEX
+
 urlpatterns = [
     url(r'^admin/', include(admin.site.urls)),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
@@ -19,10 +22,18 @@ if settings.DISCOURSE_SSO_ENABLED:
     ]
 
 urlpatterns += [
-    url(r'', include(ore.accounts.urls)),
-    url(r'', include(ore.core.urls)),
-    url(r'', include(ore.projects.urls)),
+    url(r'^users/(?P<namespace>' + EXTENDED_URL_REGEX + ')/$',
+        RedirectView.as_view(pattern_name='core-namespace', permanent=False),
+        name='users-root'
+        ),
+    url(r'^organizations/(?P<namespace>' + EXTENDED_URL_REGEX + ')/$',
+        RedirectView.as_view(pattern_name='core-namespace', permanent=False),
+        name='organizations-root'
+        ),
 
+    url(r'', include(ore.accounts.urls)),
+    url(r'', include(ore.projects.urls)),
     url(r'', include(ore.versions.urls)),
-    url(r'', include(ore.teams.urls))
+    url(r'', include(ore.teams.urls)),
+    url(r'', include(ore.core.urls)),
 ]
