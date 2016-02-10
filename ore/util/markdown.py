ff0ff1f8720a581806abc44f592eb4309d84d7d9
Luke Granger-Brown <git@lukegb.com>
2016-02-10 21:10:40 +0000

    J A V A S C R I P T

--- ore/util/markdown.py
+++ ore/util/markdown.py
@@ -10,6 +10,21 @@ def load_dukpy_ctx():
         raise ValueError("Unable to locate node_modules directory - is Ore installed correctly?")
 
     ctx = dukpy.RequirableContext([result])
+
+    def resolve_wrap(resolve):
+        def inner(req_ctx, id_, search_paths):
+            # bodge to work around the fact
+            # that the browser version of util
+            # imports inherits, which expects util to provide inherits
+            # yaey
+            if id_ == '!inherits!':
+                id_ = '!inherits/inherits_browser.js!'
+
+            return resolve(req_ctx, id_, search_paths)
+        return inner
+
+    ctx.finder.resolve = resolve_wrap(ctx.finder.resolve)
+
     return ctx
 
 
