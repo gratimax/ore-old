4036691d37d101165bd9d039d4c44dffa77e0cba
Luke Granger-Brown <git@lukegb.com>
2016-02-16 17:15:14 +0000

    Add syntax highlighting to Markdown

--- ore/core/static/ore/js/markdown.js
+++ ore/core/static/ore/js/markdown.js
@@ -1,5 +1,19 @@
 (function() {
-    var markdownIt = require('markdown-it')('commonmark');
+    var hljs = require('highlightjs/highlight.pack.min.js');
+    var markdownIt = require('markdown-it')('commonmark', {
+        highlight: function(code, lang) {
+            if (lang && hljs.getLanguage(lang)) {
+                try {
+                    return hljs.highlight(lang, code).value;
+                } catch (e) {
+                    console.error(':(', e);
+                }
+            }
+
+            return '';
+        }
+
+    });
     var markdownItEmoji = require('markdown-it-emoji');
     var sanitizeHtml = require('sanitize-html');
 
@@ -11,15 +25,28 @@
     };
 
     var renderMarkdown = function renderMarkdown(txt, options) {
-        var output = markdownIt.render(txt, options);
+        var output = markdownIt.render(txt);
 
+        var validHljs = ["hljs-addition", "hljs-attr", "hljs-attribute", "hljs-built", "hljs-builtin", "hljs-bullet", "hljs-class", "hljs-comment", "hljs-deletion", "hljs-doctag", "hljs-emphasis", "hljs-keyword", "hljs-link", "hljs-literal", "hljs-meta", "hljs-name", "hljs-number", "hljs-quote", "hljs-regexp", "hljs-section", "hljs-selector", "hljs-string", "hljs-strong", "hljs-subst", "hljs-symbol", "hljs-tag", "hljs-template", "hljs-title", "hljs-type", "hljs-variable"];
         output = sanitizeHtml(output, {
-            allowedTags: ['pre', 'code', 'ul', 'ol', 'li', 'p', 'blockquote', 'hr', 'em', 'strong', 'h1', 'h2', 'h3', 'h4', 'h5', 'h6', 'a', 'img', 'br', 'table', 'thead', 'tbody', 'tr', 'th', 'td', 'caption', 'strike', 'b', 'u', 'i'],
+            allowedTags: ['pre', 'code', 'ul', 'ol', 'li', 'p', 'blockquote', 'hr', 'em', 'strong', 'h1', 'h2', 'h3', 'h4', 'h5', 'h6', 'a', 'img', 'br', 'table', 'thead', 'tbody', 'tr', 'th', 'td', 'caption', 'strike', 'b', 'u', 'i', 'span'],
             allowedAttributes: {
                 a: ['href', 'name', 'target'],
                 img: ['src', 'title', 'alt', 'class', 'width', 'height'],
+                span: ['class'],
             },
             transformTags: {
+                'span': function(tagName, attribs) {
+                    var newAttribs = {};
+                    if (attribs.hasOwnProperty('class') && validHljs.indexOf(attribs.class) !== -1) {
+                        newAttribs.class = attribs.class;
+                    }
+
+                    return {
+                        tagName: tagName,
+                        attribs: newAttribs,
+                    };
+                },
                 'img': function(tagName, attribs) {
                     var newAttribs = {};
 
